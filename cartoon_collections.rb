fd8f577f53c10dfdee5b65fa3c95fbf866921697
Paul Rosero <paul_rosero@yahoo.com>
2019-04-02 22:54:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
-  i = 0
-  dwarves.each_with_index { |dwarves, i| puts "#{i += 1}.#{dwarves}" }
+
+  dwarves.each_with_index { |dwarves, i| puts "#{i += 1}. #{dwarves}" }
 end
 
 def summon_captain_planet(veggies)
